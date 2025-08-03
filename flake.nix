45c9847940bcb0ac387573d8091b7c7fad2b0272
Mounir Nessim <mmnessim@gmail.com>
2025-08-03 19:58:16 +0000

    update flake.nix -> the-nix-way devtemplates

--- flake.nix
+++ flake.nix
@@ -1,21 +1,53 @@
 {
-  description = "A very basic flake";
+  description = "A Nix-flake-based Java development environment";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs = { self, nixpkgs }: {
+  outputs = inputs:
+    let
+      javaVersion = 23; # Change this value to update the whole stack
 
-    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = [
-        nixpkgs.legacyPackages.x86_64-linux.openjdk
-        nixpkgs.legacyPackages.x86_64-linux.gradle
-      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ inputs.self.overlays.default ];
+        };
+      });
+    in
+    {
+      overlays.default = final: prev:
+        let
+          jdk = prev."jdk${toString javaVersion}";
+        in
+        {
+          inherit jdk;
+          maven = prev.maven.override { jdk_headless = jdk; };
+          gradle = prev.gradle.override { java = jdk; };
+          lombok = prev.lombok.override { inherit jdk; };
+        };
 
-      shellHook = ''
-        export PS1="(nix-shell) $PS1"
-      '';
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            gradle
+            jdk
+            maven
+            ncurses
+            patchelf
+            zlib
+          ];
+
+          shellHook =
+            let
+              loadLombok = "-javaagent:${pkgs.lombok}/share/java/lombok.jar";
+              prev = "\${JAVA_TOOL_OPTIONS:+ $JAVA_TOOL_OPTIONS}";
+            in
+            ''
+              export JAVA_TOOL_OPTIONS="${loadLombok}${prev}"
+            '';
+        };
+      });
     };
-  };
 }
